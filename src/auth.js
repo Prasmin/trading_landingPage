a606c194a175d8b6dd361130797770d6b6cdfc4b
Prashmin Gurung <prashmingrg@gmail.com>
2025-02-17 12:20:06 +1100

    still working in authenticaiton

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import Resend from "next-auth/providers/resend";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -7,9 +8,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-  ],
 
-  pages: {
-    signIn: "/sign-in",
-  },
+    Resend,
+  ],
 });
